a0e31ca5f9defe996c6d176feaf5e807c2b17c9b
mhyckaherviananda12 <95332273+mhyckaherviananda12@users.noreply.github.com>
2021-12-08 19:19:50 +0800

    Update Lord.js

--- Lord.js
+++ Lord.js
@@ -439,17 +439,15 @@ var waktoo = date.getHours();
 				mess = {
 					wait: 'Proses Lord..',
 					limit: 'Limit kamu habis lord , Silahkan beli dengan #buylimit <nominal> atau minta gift dari user lain',
-					sewabot: 
-sendButLocation(from, ` 「 *SEWA BOT* 」 \n\nBerikut list sewa bot kami
-					
-➳ 1 MINGGU = Rp 10.000
+					sewabot: ` 「 *SEWA BOT* 」 \n\nBerikut list sewa bot kami
+
+•➳ 1 MINGGU = Rp 10.000
 ➳ 2 MINGGU = Rp 15.000
 ➳ 3 MINGGU = Rp 20.000
 ➳ 1 BULAN   = Rp 25.000
 
 MINAT? PM:
-_wa.me/${owner_number}_`,) 
-										break
+_wa.me/${owner_number}_`,
 					ban: 'Maaf Lord Nomor Kamu Sudah Di Banned!',
 					error: 'Error Lord, Mungkin Lagi Diperbaiki',
 					errorLink: 'Link Error Lord',
