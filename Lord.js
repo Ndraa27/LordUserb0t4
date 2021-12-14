0f576f90b7f2f22651e161a557f4d7562d0ed2cc
mhyckaherviananda12 <95332273+mhyckaherviananda12@users.noreply.github.com>
2021-12-14 17:47:30 +0800

    Update Lord.js

--- Lord.js
+++ Lord.js
@@ -1818,9 +1818,7 @@ Alasan : ${reason}`, [sender], true)
 									break
 						case 'donasi':
 									reply('╭─❒ *Donation* ❒\n│➳ *6285340409831*\n│➳ *bit.ly/mhyckaaa*\n | ➳ *bit.ly/mhyckaherviananda*\n╰❒ *Thank you*') 
-                                                                       gam = fs.readFileSync('./image/nasa by mhycka.jpg')
-                                                                       sendButLocation(from, "© BotWhatsApp By ᴹᴿ°᭄Mhyckaོ", gam,) 
-
+                                                               
 									break
 						case 'join':case 'joingc':
 									if (!q) return reply('Linknya?')
